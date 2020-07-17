3c5f869d589c359ffee52653d770c1d1e39ca207
Dong Xia <dong7777xia@gmail.com>
2020-07-17 13:37:39 +0000

    Automatically backed up by Learn

--- nested.rb
+++ nested.rb
@@ -104,9 +104,9 @@ def changing_alan
       }
     }
 
-		programmer_hash[:yukihiro_matsumoto] = {
-		   :known_for => "Ruby",
-		   :languages => ["LISP", "C"]
+	programmer_hash[:yukihiro_matsumoto] = {
+		  :known_for => "Ruby",
+		  :languages => ["LISP", "C"]
 		 }
 
 		programmer_hash 
